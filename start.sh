1c14639cf851c71b772118fa33fa012bbd14f990
tststs <contact@tobias-schubert.com>
2021-03-16 10:48:13 +0100

    limit logging

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./bin/linux/nano-work-server --cpu-threads 1 -l 127.0.0.1:7000 > nano-work-server.log &
-python3 bpow_client.py --payout ban_3jcr7qrykbtyu8x6fik1tq7o77pstnwo7hmsqoswjzzwky5hjo4ofgza657y --async_mode --work any
+./bin/linux/nano-work-server --cpu-threads 1 -l 127.0.0.1:7000 > nano-work-server.log 2>&1 &
+python3 bpow_client.py --payout ban_3jcr7qrykbtyu8x6fik1tq7o77pstnwo7hmsqoswjzzwky5hjo4ofgza657y --limit-logging --async_mode --work any
